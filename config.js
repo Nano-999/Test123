71bb441345839c70fce583d051f81b9da8290adc
Nano-999 <thaktharikita@gmail.com>
2025-07-13 19:58:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
-ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
+SESSION_ID: process.env.SESSION_ID || "fpxQ2LgA#nP57RDLT48aLm6sEatHUKX9GnrrtnJ-7S8jk92QV7Sg", // session id
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false", // true,false
+ANTI_DELETE: process.env.ANTI_DELETE || "false", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
 ALLWAYS_OFFLINE: process.env.ALLWAYS_OFFLINE || "false", // true,false
 LANG: process.env.LANG || "EN",
